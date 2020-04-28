140b8992de84d61d88e494f040d967e498454338
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-04-28 09:56:59 +0900

    Initial Commit

---
+++ coffeeList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coffeeList
+//
+//  Created by JU HAN LEE on 2020/04/28.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
